a9b0d0c187b219373a18b496516409b5cc8bab39
Ananggi Widian <ananggiwidian@gmail.com>
2026-04-24 21:26:54 +0700

    fix : prisma generate

--- app/api/stock/route.ts
+++ app/api/stock/route.ts
@@ -2,14 +2,34 @@ import { NextRequest, NextResponse } from "next/server";
 import { prisma } from "@/lib/prisma";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { Prisma } from "@prisma/client";
 
 export async function POST(req: NextRequest) {
   const session = await getServerSession(authOptions);
-  if (!session || session.user.role !== "ADMIN") return NextResponse.json({ error: "Forbidden" }, { status: 403 });
+  if (!session || session.user.role !== "ADMIN")
+    return NextResponse.json({ error: "Forbidden" }, { status: 403 });
+
   const { productId, quantity, type, reference } = await req.json();
-  await prisma.$transaction(async (tx) => {
-    await tx.stockMovement.create({ data: { productId, quantity, type, reference, userId: session.user.id } });
-    await tx.product.update({ where: { id: productId }, data: { stock: { increment: type === "IN" ? quantity : -quantity } } });
+
+  await prisma.$transaction(async (tx: Prisma.TransactionClient) => {
+    await tx.stockMovement.create({
+      data: {
+        productId,
+        quantity,
+        type,
+        reference,
+        userId: session.user.id,
+      },
+    });
+    await tx.product.update({
+      where: { id: productId },
+      data: {
+        stock: {
+          increment: type === "IN" ? quantity : -quantity,
+        },
+      },
+    });
   });
+
   return NextResponse.json({ success: true });
 }
--- app/api/transactions/route.ts
+++ app/api/transactions/route.ts
@@ -2,6 +2,7 @@ import { NextRequest, NextResponse } from "next/server";
 import { prisma } from "@/lib/prisma";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { Prisma } from "@prisma/client";
 
 export async function POST(req: NextRequest) {
   const session = await getServerSession(authOptions);
@@ -9,7 +10,7 @@ export async function POST(req: NextRequest) {
   const { items, total, paymentMethod } = await req.json();
   const invoiceNo = `INV-${Date.now()}`;
   try {
-    const result = await prisma.$transaction(async (tx) => {
+    const result = await prisma.$transaction(async (tx : Prisma.TransactionClient) => {
       const transaction = await tx.transaction.create({ data: { invoiceNo, userId: session.user.id, total, paymentMethod, status: "COMPLETED" } });
       for (const item of items) {
         await tx.transactionItem.create({ data: { transactionId: transaction.id, productId: item.productId, quantity: item.quantity, priceAtTime: item.priceAtTime } });
